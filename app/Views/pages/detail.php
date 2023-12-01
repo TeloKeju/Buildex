31f181f321a8eeee05c929f356be56b03680fd4b
Dani Ardiansyach <107809518+TeloKeju@users.noreply.github.com>
2023-12-01 12:25:59 +0700

    Update detail.php

---
+++ app/Views/pages/detail.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<main>
+    <article>
+        <section>
+            <h1>Hellow World</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, error? Quibusdam fuga molestiae nihil deleniti quis sapiente eius, voluptatem a consequuntur blanditiis eum perferendis. Illo asperiores culpa corporis nam eveniet!</p>
+        </section>
+    </article>
+</main>
+<?= $this->endSection(); ?>

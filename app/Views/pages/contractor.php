f386fb4697b7dd31e299db743b9fb15ac7c29321
Dani Ardiansyach <107809518+TeloKeju@users.noreply.github.com>
2023-12-09 13:49:41 +0700

    Update fetch contractor.json

--- app/Views/pages/contractor.php
+++ app/Views/pages/contractor.php
@@ -3,61 +3,15 @@
 <?= $this->section('content'); ?>
 <main class="p-3">
     <h1 class="text-center fs-5 mt-5" id="test">Detail Kontraktor</h1>
-    <article class="content-contractor p-3">
-        <section class="card p-3">
-            <img src="https://placehold.co/300x300" class="rounded mx-auto d-block img-fluid" alt="Foto Kontraktor">
-            <h1 class="fw-bold fs-5 mt-2">Nama: Walter White</h1>
-            <h2 class="fw-medium fs-6">Umur: 30</h2>
-            <p class="fs-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi tempora molestiae, iste quia quos veritatis, nulla ipsam officia temporibus obcaecati ad autem et quidem modi architecto rerum. Vel, dolorem labore.</p>
-        </section>
-        <section class="card p-3">
-            <img src="https://placehold.co/300x300" class="rounded mx-auto d-block img-fluid" alt="Foto Kontraktor">
-            <h1 class="fw-bold fs-5 mt-2">Nama: Walter White</h1>
-            <h2 class="fw-medium fs-6">Umur: 30</h2>
-            <p class="fs-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi tempora molestiae, iste quia quos veritatis, nulla ipsam officia temporibus obcaecati ad autem et quidem modi architecto rerum. Vel, dolorem labore.</p>
-        </section>
-        <section class="card p-3">
-            <img src="https://placehold.co/300x300" class="rounded mx-auto d-block img-fluid" alt="Foto Kontraktor">
-            <h1 class="fw-bold fs-5 mt-2">Nama: Walter White</h1>
-            <h2 class="fw-medium fs-6">Umur: 30</h2>
-            <p class="fs-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi tempora molestiae, iste quia quos veritatis, nulla ipsam officia temporibus obcaecati ad autem et quidem modi architecto rerum. Vel, dolorem labore.</p>
-        </section>
-        <section class="card p-3">
-            <img src="https://placehold.co/300x300" class="rounded mx-auto d-block img-fluid" alt="Foto Kontraktor">
-            <h1 class="fw-bold fs-5 mt-2">Nama: Walter White</h1>
-            <h2 class="fw-medium fs-6">Umur: 30</h2>
-            <p class="fs-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi tempora molestiae, iste quia quos veritatis, nulla ipsam officia temporibus obcaecati ad autem et quidem modi architecto rerum. Vel, dolorem labore.</p>
-        </section>
-        <section class="card p-3">
-            <img src="https://placehold.co/300x300" class="rounded mx-auto d-block img-fluid" alt="Foto Kontraktor">
-            <h1 class="fw-bold fs-5 mt-2">Nama: Walter White</h1>
-            <h2 class="fw-medium fs-6">Umur: 30</h2>
-            <p class="fs-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi tempora molestiae, iste quia quos veritatis, nulla ipsam officia temporibus obcaecati ad autem et quidem modi architecto rerum. Vel, dolorem labore.</p>
-        </section>
-        <section class="card p-3">
-            <img src="https://placehold.co/300x300" class="rounded mx-auto d-block img-fluid" alt="Foto Kontraktor">
-            <h1 class="fw-bold fs-5 mt-2">Nama: Walter White</h1>
-            <h2 class="fw-medium fs-6">Umur: 30</h2>
-            <p class="fs-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi tempora molestiae, iste quia quos veritatis, nulla ipsam officia temporibus obcaecati ad autem et quidem modi architecto rerum. Vel, dolorem labore.</p>
-        </section>
-        <section class="card p-3">
-            <img src="https://placehold.co/300x300" class="rounded mx-auto d-block img-fluid" alt="Foto Kontraktor">
-            <h1 class="fw-bold fs-5 mt-2">Nama: Walter White</h1>
-            <h2 class="fw-medium fs-6">Umur: 30</h2>
-            <p class="fs-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi tempora molestiae, iste quia quos veritatis, nulla ipsam officia temporibus obcaecati ad autem et quidem modi architecto rerum. Vel, dolorem labore.</p>
-        </section>
-        <section class="card p-3">
-            <img src="https://placehold.co/300x300" class="rounded mx-auto d-block img-fluid" alt="Foto Kontraktor">
-            <h1 class="fw-bold fs-5 mt-2">Nama: Walter White</h1>
-            <h2 class="fw-medium fs-6">Umur: 30</h2>
-            <p class="fs-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi tempora molestiae, iste quia quos veritatis, nulla ipsam officia temporibus obcaecati ad autem et quidem modi architecto rerum. Vel, dolorem labore.</p>
-        </section>
-        <section class="card p-3">
-            <img src="https://placehold.co/300x300" class="rounded mx-auto d-block img-fluid" alt="Foto Kontraktor">
-            <h1 class="fw-bold fs-5 mt-2">Nama: Walter White</h1>
-            <h2 class="fw-medium fs-6">Umur: 30</h2>
-            <p class="fs-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi tempora molestiae, iste quia quos veritatis, nulla ipsam officia temporibus obcaecati ad autem et quidem modi architecto rerum. Vel, dolorem labore.</p>
-        </section>
+    <article class="content-contractor">
+        <?php foreach ($contractors as $contractor) : ?>
+            <section class="card p-3">
+                <img src="https://placehold.co/300x300" class="rounded mx-auto d-block img-fluid" alt="Foto Kontraktor">
+                <h1 class="fw-bold fs-5 mt-2">Nama: <?= $contractor['nama']; ?></h1>
+                <h2 class="fw-medium fs-6">Umur: <?= $contractor['umur']; ?></h2>
+                <p class="fs-6"><?= $contractor['description']; ?></p>
+            </section>
+        <?php endforeach; ?>
     </article>
 </main>
 <?= $this->endSection(); ?>

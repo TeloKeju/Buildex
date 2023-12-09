50bae3def331cbddba05f0d9824275f1fc95a3e2
Dani Ardiansyach <107809518+TeloKeju@users.noreply.github.com>
2023-12-09 14:34:29 +0700

    Update fetch rumah.json

--- app/Views/pages/desain.php
+++ app/Views/pages/desain.php
@@ -4,87 +4,17 @@
 <main class="p-3">
     <h1 class="text-center fs-5 mt-5" id="test">Detail Rumah</h1>
     <article class="content-desain">
-        <section class="card mt-2">
-            <img src="https://placehold.co/300x300" class="img-fluid m-0" alt="Foto Kontraktor">
-            <section class="p-2">
-                <h1 class="fw-bold fs-5 mt-2">The Zora Cluster Kanade</h1>
-                <h2 class="fw-light fs-6">Pagedangan, Tangerang</h2>
-                <h3 class="fw-normal fs-6">Luas Bangunan: <span class="fw-medium">Dari 358 - 485m²</span></h3>
-                <h3 class="fw-normal fs-6">Luas Tanah: <span class="fw-medium">Dari 200 - 264m²</span></h3>
+        <?php foreach ($rumah as $item) : ?>
+            <section class="card mt-2">
+                <img src="https://placehold.co/300x300" class="img-fluid m-0" alt="Foto Rumah">
+                <section class="p-2">
+                    <h1 class="fw-bold fs-5 mt-2"><a href="pages/detail"><?= $item['title']; ?></a></h1>
+                    <h2 class="fw-light fs-6"><?= $item['kota']; ?></h2>
+                    <h3 class="fw-normal fs-6">Luas Bangunan: <span class="fw-medium"><?= $item['luasBangunan']; ?></span></h3>
+                    <h3 class="fw-normal fs-6">Luas Tanah: <span class="fw-medium"><?= $item['luasTanah']; ?></span></h3>
+                </section>
             </section>
-        </section>
-        <section class="card mt-2">
-            <img src="https://placehold.co/300x300" class="img-fluid m-0" alt="Foto Kontraktor">
-            <section class="p-2">
-                <h1 class="fw-bold fs-5 mt-2">The Zora Cluster Kanade</h1>
-                <h2 class="fw-light fs-6">Pagedangan, Tangerang</h2>
-                <h3 class="fw-normal fs-6">Luas Bangunan: <span class="fw-medium">Dari 358 - 485m²</span></h3>
-                <h3 class="fw-normal fs-6">Luas Tanah: <span class="fw-medium">Dari 200 - 264m²</span></h3>
-            </section>
-        </section>
-        <section class="card mt-2">
-            <img src="https://placehold.co/300x300" class="img-fluid m-0" alt="Foto Kontraktor">
-            <section class="p-2">
-                <h1 class="fw-bold fs-5 mt-2">The Zora Cluster Kanade</h1>
-                <h2 class="fw-light fs-6">Pagedangan, Tangerang</h2>
-                <h3 class="fw-normal fs-6">Luas Bangunan: <span class="fw-medium">Dari 358 - 485m²</span></h3>
-                <h3 class="fw-normal fs-6">Luas Tanah: <span class="fw-medium">Dari 200 - 264m²</span></h3>
-            </section>
-        </section>
-        <section class="card mt-2">
-            <img src="https://placehold.co/300x300" class="img-fluid m-0" alt="Foto Kontraktor">
-            <section class="p-2">
-                <h1 class="fw-bold fs-5 mt-2">The Zora Cluster Kanade</h1>
-                <h2 class="fw-light fs-6">Pagedangan, Tangerang</h2>
-                <h3 class="fw-normal fs-6">Luas Bangunan: <span class="fw-medium">Dari 358 - 485m²</span></h3>
-                <h3 class="fw-normal fs-6">Luas Tanah: <span class="fw-medium">Dari 200 - 264m²</span></h3>
-            </section>
-        </section>
-        <section class="card mt-2">
-            <img src="https://placehold.co/300x300" class="img-fluid m-0" alt="Foto Kontraktor">
-            <section class="p-2">
-                <h1 class="fw-bold fs-5 mt-2">The Zora Cluster Kanade</h1>
-                <h2 class="fw-light fs-6">Pagedangan, Tangerang</h2>
-                <h3 class="fw-normal fs-6">Luas Bangunan: <span class="fw-medium">Dari 358 - 485m²</span></h3>
-                <h3 class="fw-normal fs-6">Luas Tanah: <span class="fw-medium">Dari 200 - 264m²</span></h3>
-            </section>
-        </section>
-        <section class="card mt-2">
-            <img src="https://placehold.co/300x300" class="img-fluid m-0" alt="Foto Kontraktor">
-            <section class="p-2">
-                <h1 class="fw-bold fs-5 mt-2">The Zora Cluster Kanade</h1>
-                <h2 class="fw-light fs-6">Pagedangan, Tangerang</h2>
-                <h3 class="fw-normal fs-6">Luas Bangunan: <span class="fw-medium">Dari 358 - 485m²</span></h3>
-                <h3 class="fw-normal fs-6">Luas Tanah: <span class="fw-medium">Dari 200 - 264m²</span></h3>
-            </section>
-        </section>
-        <section class="card mt-2">
-            <img src="https://placehold.co/300x300" class="img-fluid m-0" alt="Foto Kontraktor">
-            <section class="p-2">
-                <h1 class="fw-bold fs-5 mt-2">The Zora Cluster Kanade</h1>
-                <h2 class="fw-light fs-6">Pagedangan, Tangerang</h2>
-                <h3 class="fw-normal fs-6">Luas Bangunan: <span class="fw-medium">Dari 358 - 485m²</span></h3>
-                <h3 class="fw-normal fs-6">Luas Tanah: <span class="fw-medium">Dari 200 - 264m²</span></h3>
-            </section>
-        </section>
-        <section class="card mt-2">
-            <img src="https://placehold.co/300x300" class="img-fluid m-0" alt="Foto Kontraktor">
-            <section class="p-2">
-                <h1 class="fw-bold fs-5 mt-2">The Zora Cluster Kanade</h1>
-                <h2 class="fw-light fs-6">Pagedangan, Tangerang</h2>
-                <h3 class="fw-normal fs-6">Luas Bangunan: <span class="fw-medium">Dari 358 - 485m²</span></h3>
-                <h3 class="fw-normal fs-6">Luas Tanah: <span class="fw-medium">Dari 200 - 264m²</span></h3>
-            </section>
-        </section>
-        <section class="card mt-2">
-            <img src="https://placehold.co/300x300" class="img-fluid m-0" alt="Foto Kontraktor">
-            <section class="p-2">
-                <h1 class="fw-bold fs-5 mt-2">The Zora Cluster Kanade</h1>
-                <h2 class="fw-light fs-6">Pagedangan, Tangerang</h2>
-                <h3 class="fw-normal fs-6">Luas Bangunan: <span class="fw-medium">Dari 358 - 485m²</span></h3>
-                <h3 class="fw-normal fs-6">Luas Tanah: <span class="fw-medium">Dari 200 - 264m²</span></h3>
-            </section>
-        </section>
+        <?php endforeach; ?>
     </article>
 </main>
 <?= $this->endSection(); ?>

8352288cbe863fd236807eb1c0d754a1672e9b9c
Dani Ardiansyach <107809518+TeloKeju@users.noreply.github.com>
2023-12-11 00:45:43 +0700

    Update template.php

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/styles/style.css">
 </head>
 
 <body>
@@ -20,14 +21,14 @@
             <nav class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        <a class="nav-link active" aria-current="page" href="home">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" href="#about">About Us</a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link active">Login</a>
+                        <a class="nav-link active" href="login">Login</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
@@ -48,7 +49,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 
-    <footer class="mx-auto p-2">
+    <footer class="footer mt-auto py-3 bg-light">
         <p class="text-center">&copy;Copyright 2023</p>
     </footer>
 </body>

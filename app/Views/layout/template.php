9714e808e3cbad1129503260a47f52ec50cf3674
ArviNanda <142085523+ArviNanda@users.noreply.github.com>
2023-12-11 22:18:30 +0700

    Update template.php

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -21,14 +21,14 @@
             <nav class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="home">Home</a>
+                        <a class="nav-link active" aria-current="page" href="<?= base_url('pages/home') ?>">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" href="#about">About Us</a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link active" href="login">Login</a>
+                        <a class="nav-link active" href="<?= base_url('pages/login') ?>">Login</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">

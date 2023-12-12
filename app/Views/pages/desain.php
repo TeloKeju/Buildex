5e3960bd7275e77d2414581c413b036b986abc1e
najmil <107398498+najmil@users.noreply.github.com>
2023-12-12 21:50:35 +0700

    Update desain.php

--- app/Views/pages/desain.php
+++ app/Views/pages/desain.php
@@ -6,7 +6,7 @@
     <article class="content-desain">
         <?php foreach ($rumah as $item) : ?>
             <section class="card mt-2">
-                <img src="https://placehold.co/300x300" class="img-fluid m-0" alt="Foto Rumah">
+                <img src="<?= base_url($item['rumah']); ?>" class="img-fluid m-0" alt="Foto Rumah">
                 <section class="p-2">
                     <h1 class="fw-bold fs-5 mt-2"><a href="<?= base_url('detail/' . $item['id']); ?>"><?= $item['title']; ?></a></h1>
                     <h2 class="fw-light fs-6"><?= $item['kota']; ?></h2>

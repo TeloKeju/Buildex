b3d41a15c4a71660d7c13a044b32d381b62b9fc3
najmil <107398498+najmil@users.noreply.github.com>
2023-12-13 23:09:36 +0700

    Update login.php

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -4,15 +4,15 @@
 <main class="p-3">
     <form>
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <label for="loginEmail" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="loginEmail" aria-describedby="emailHelp">
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <label for="loginPassword" class="form-label">Password</label>
+            <input type="password" class="form-control" id="loginPassword">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary" id="btnLogin">Log In</button>
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#pendaftaran">
             Daftar Sekarang
         </button>
@@ -30,46 +30,119 @@
                         <div class="input-group-prepend">
                             <span class="input-group-text">Username anda</span>
                         </div>
-                        <input type="text" class="form-control nama" placeholder="username anda" />
+                        <input type="text" class="form-control nama" id="username" placeholder="username anda" />
                     </div>
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
                             <span class="input-group-text">Nama Lengkap</span>
                         </div>
-                        <input type="text" class="form-control nama" placeholder="Nama Anda" />
+                        <input type="text" class="form-control nama" id="nama_lengkap" placeholder="Nama Anda" />
                     </div>
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
                             <span class="input-group-text">Email</span>
                         </div>
-                        <input type="text" class="form-control kelas" placeholder="Email anda" />
+                        <input type="text" class="form-control kelas" id="email" placeholder="Email anda" />
                     </div>
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
                             <span class="input-group-text">No Telephone</span>
                         </div>
-                        <input type="text" class="form-control kelas" placeholder="Nomor Telepone anda" />
+                        <input type="text" class="form-control kelas" id="no_telephone" placeholder="Nomor Telepone anda" />
                     </div>
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
                             <span class="input-group-text">Password</span>
                         </div>
-                        <input type="text" class="form-control kelas" placeholder="Masukkan Password anda" />
+                        <input type="password" class="form-control kelas" id="password" placeholder="Masukkan Password anda" />
                     </div>
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
                             <span class="input-group-text"> Konfirmasi Password</span>
                         </div>
-                        <input type="text" class="form-control kelas" placeholder="Passwordnya disamakan" />
+                        <input type="password" class="form-control kelas" placeholder="Passwordnya disamakan" />
                     </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Daftar</button>
+                    <button type="button" class="btn btn-primary" id="btnDaftar">Daftar</button>
                 </div>
             </div>
         </div>
     </div>
 
 </main>
+
+<script>
+    $(document).ready(function() {
+        $('#btnDaftar').click(function() {
+            var username = $('#username').val();
+            var nama_lengkap = $('#nama_lengkap').val();
+            var email = $('#email').val();
+            var no_telephone = $('#no_telephone').val();
+            var password = $('#password').val();
+
+            $.ajax({
+                type: 'POST',
+                url: '<?= base_url('login/register'); ?>',
+                data: {
+                    username: username,
+                    nama_lengkap: nama_lengkap,
+                    email: email,
+                    no_telephone: no_telephone,
+                    password: password
+                },
+                dataType: 'json',
+                success: function(response) {
+                    if (response.status === 'success') {
+                        console.log(response.message);
+                        // window.location = '<?php //echo base_url('pages/index'); 
+                                                ?>';
+                        setTimeout(function() {
+                            toastr.success(response.message, 'Berhasil Register!');
+                            location.reload();
+                        }, 2000);
+                    } else {
+                        alert(response.message);
+                    }
+                },
+                error: function() {
+                    alert('Terjadi kesalahan.');
+                }
+            });
+        });
+
+        $('#btnLogin').click(function() {
+            var loginEmail = $('#loginEmail').val();
+            var loginPassword = $('#loginPassword').val();
+            console.log('loginEmail', loginEmail);
+            console.log('loginPassword', loginPassword);
+
+            $.ajax({
+                type: 'POST',
+                url: '<?= base_url('login/login'); ?>',
+                data: {
+                    email: loginEmail,
+                    password: loginPassword
+                },
+                dataType: 'json',
+                success: function(response) {
+                    if (response.status === 'success') {
+                        console.log(response.message);
+                        setTimeout(function() {
+                            toastr.success(response.message, 'Berhasil Login!');
+                            window.location = '<?= base_url('pages/index');
+                                                ?>';
+                        }, 2000);
+                    } else {
+                        alert(response.message);
+                    }
+                },
+                error: function() {
+                    alert('Terjadi kesalahan.');
+                }
+            });
+        });
+    });
+</script>
 <?= $this->endSection(); ?>

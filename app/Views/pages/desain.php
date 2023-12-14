f0a931fff8301c44a084f5ee59390c105ee5f6c4
Dani Ardiansyach <107809518+TeloKeju@users.noreply.github.com>
2023-12-14 18:02:42 +0700

    Update desain.php

--- app/Views/pages/desain.php
+++ app/Views/pages/desain.php
@@ -6,13 +6,12 @@
     <article class="content-desain">
         <?php foreach ($rumah as $item) : ?>
             <section class="card mt-2">
-                <a href=""></a>
                 <img src="<?= base_url($item['rumah']); ?>" class="img-fluid m-0" alt="Foto Rumah">
                 <section class="p-2">
-                    <h1 class="fw-bold fs-5 mt-2"><a href="<?= base_url('detail/' . $item['id']); ?>"><?= $item['title']; ?></a></h1>
-                    <h2 class="fw-light fs-6"><?= $item['kota']; ?></h2>
-                    <h3 class="fw-normal fs-6">Luas Bangunan: <span class="fw-medium"><?= $item['luasBangunan']; ?></span></h3>
-                    <h3 class="fw-normal fs-6">Luas Tanah: <span class="fw-medium"><?= $item['luasTanah']; ?></span></h3>
+                    <h1 class="fw-bold fs-5 mt-2 ms-2"><a href="<?= base_url('detail/' . $item['id']); ?>"><?= $item['title']; ?></a></h1>
+                    <h2 class="fw-light fs-6 p-2"><?= $item['kota']; ?></h2>
+                    <h3 class="fw-normal fs-6 ms-2">Luas Bangunan: <span class="fw-medium"><?= $item['luasBangunan']; ?></span></h3>
+                    <h3 class="fw-normal fs-6 ms-2">Luas Tanah: <span class="fw-medium"><?= $item['luasTanah']; ?></span></h3>
                 </section>
             </section>
         <?php endforeach; ?>

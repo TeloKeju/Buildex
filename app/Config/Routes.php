94971c7abec1615efb1e20dd9d1620da3b1d7cf0
najmil <107398498+najmil@users.noreply.github.com>
2023-12-17 10:32:42 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,6 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Pages::index');
 $routes->setAutoRoute(true);
 $routes->get('detail/(:num)', 'Pages::detail/$1');
+$routes->post('login/logout', 'Login::logout');
+$routes->get('login/logout', 'Login::logout');
+$routes->get('pages/detailpesanan/(:num)', 'Pages::detailpesanan/$1');

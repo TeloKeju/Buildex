68414c3d6240e8bfecb764cd01385a1afe4093a6
Dani Ardiansyach <107809518+TeloKeju@users.noreply.github.com>
2023-12-18 07:20:07 +0700

    Update desain.php

--- app/Views/pages/desain.php
+++ app/Views/pages/desain.php
@@ -2,16 +2,16 @@
 
 <?= $this->section('content'); ?>
 <main class="p-3">
-    <h1 class="text-center fs-5 mt-5" id="test">Detail Rumah</h1>
+    <h1 class="text-center fs-5 mt-5" id="test" tabindex="0">Detail Rumah</h1>
     <article class="content-desain">
         <?php foreach ($rumah as $item) : ?>
             <section class="card mt-2">
                 <img src="<?= base_url($item['rumah']); ?>" class="img-fluid m-0" alt="Foto Rumah">
                 <section class="p-2">
-                    <h1 class="fw-bold fs-5 mt-2 ms-2"><a href="<?= base_url('detail/' . $item['id']); ?>"><?= $item['title']; ?></a></h1>
-                    <h2 class="fw-light fs-6 p-2"><?= $item['kota']; ?></h2>
-                    <h3 class="fw-normal fs-6 ms-2">Luas Bangunan: <span class="fw-medium"><?= $item['luasBangunan']; ?></span></h3>
-                    <h3 class="fw-normal fs-6 ms-2">Luas Tanah: <span class="fw-medium"><?= $item['luasTanah']; ?></span></h3>
+                    <h1 class="fw-bold fs-5 mt-2 ms-2"><a href="<?= base_url('detail/' . $item['id']); ?>" tabindex="0"><?= $item['title']; ?></a></h1>
+                    <h2 class="fw-light fs-6 p-2" tabindex="0"><?= $item['kota']; ?></h2>
+                    <h3 class="fw-normal fs-6 ms-2">Luas Bangunan: <span class="fw-medium" tabindex="0"><?= $item['luasBangunan']; ?></span></h3>
+                    <h3 class="fw-normal fs-6 ms-2">Luas Tanah: <span class="fw-medium" tabindex="0"><?= $item['luasTanah']; ?></span></h3>
                 </section>
             </section>
         <?php endforeach; ?>

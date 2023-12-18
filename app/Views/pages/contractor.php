070e1104206a640fa9ed6d76881f73651e2c6fd8
Dani Ardiansyach <107809518+TeloKeju@users.noreply.github.com>
2023-12-18 07:17:00 +0700

    Update contractor.php

--- app/Views/pages/contractor.php
+++ app/Views/pages/contractor.php
@@ -2,13 +2,13 @@
 
 <?= $this->section('content'); ?>
 <main class="p-3">
-    <h1 class="text-center fs-5 mt-5" id="test">Detail Kontraktor</h1>
+    <h1 class="text-center fs-5 mt-5" id="test" tabindex="0">Detail Kontraktor</h1>
     <article class="content-contractor">
         <?php foreach ($contractors as $contractor) : ?>
             <section class="card p-3">
                 <img src="<?= base_url($contractor['logo']); ?>" class="rounded mx-auto d-block img-fluid" alt="Foto Kontraktor">
-                <h1 class="fw-bold fs-5 mt-3"><?= $contractor['nama']; ?></h1>
-                <p class="fs-6"><?= $contractor['description']; ?></p>
+                <h1 class="fw-bold fs-5 mt-3" tabindex="0"><?= $contractor['nama']; ?></h1>
+                <p class="fs-6" tabindex="0"><?= $contractor['description']; ?></p>
             </section>
         <?php endforeach; ?>
     </article>
